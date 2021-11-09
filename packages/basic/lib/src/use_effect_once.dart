f6e8ea1899051027fa6757ef794b2b3a216fd563
wasabeef <dadadada.chop@gmail.com>
2021-11-09 19:07:47 +0900

    refactor: replace to VoidCallback

--- packages/basic/lib/src/use_effect_once.dart
+++ packages/basic/lib/src/use_effect_once.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 /// A modified [useEffect](ref link) hook that only runs once.
 /// [ref link](https://pub.dev/documentation/flutter_hooks/latest/flutter_hooks/useEffect.html)
-void useEffectOnce(void Function()? Function() effect) {
+void useEffectOnce(VoidCallback? Function() effect) {
   return useEffect(effect, []);
 }
