a4fd2207ac47b655469bd6dea98337d77fb76ac0
wasabeef <dadadada.chop@gmail.com>
2021-11-17 00:56:33 +0900

    refactor: add immutable

--- packages/basic/lib/src/use_default.dart
+++ packages/basic/lib/src/use_default.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 /// Flutter state hook that returns the default value when state is null.
 DefaultState<T> useDefault<T>(T defaultValue, T initialValue) {
   final value = useState(initialValue);
+
   final getter = useCallback<T Function()>(() {
     return value.value;
   }, const []);
@@ -12,11 +14,13 @@ DefaultState<T> useDefault<T>(T defaultValue, T initialValue) {
   }, const []);
 
   final state = useRef(DefaultState<T>(getter, setter));
+
   return state.value;
 }
 
+@immutable
 class DefaultState<T> {
-  DefaultState(this._getter, this._setter);
+  const DefaultState(this._getter, this._setter);
 
   final T Function() _getter;
   final void Function(T?) _setter;
