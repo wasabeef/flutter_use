50c312f9d290be54282c9b4ed6eff3544525ec7f
wasabeef <dadadada.chop@gmail.com>
2021-11-18 12:16:46 +0900

    refactor: change args to flutter_hooks's Dispose

--- packages/basic/lib/src/use_update_effect.dart
+++ packages/basic/lib/src/use_update_effect.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_use/src/use_first_mount_state.dart';
 
 /// Flutter effect hook that ignores the first invocation (e.g. on mount).
 /// The signature is exactly the same as the useEffect hook.
-void useUpdateEffect(VoidCallback? Function() effect, [List<Object?>? keys]) {
+void useUpdateEffect(Dispose? Function() effect, [List<Object?>? keys]) {
   final isFirstMount = useFirstMountState();
 
   useEffect(() {
