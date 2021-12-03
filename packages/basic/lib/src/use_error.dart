759d7cd99b9cb3d6071a0c7e93bf8c8def9b2729
wasabeef <dadadada.chop@gmail.com>
2021-12-03 16:01:38 +0900

    refactor: fix commit worng

--- packages/basic/lib/src/use_error.dart
+++ packages/basic/lib/src/use_error.dart
@@ -4,7 +4,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 /// Returns an error dispatcher.
 ErrorState useError() {
   final error = useState<Error?>(null);
-  final dispatcher = useCallback<_Dispatcher>((e) {
   final dispatcher = useCallback<void Function(Error e)>((e) {
     error.value = e;
   }, const []);
