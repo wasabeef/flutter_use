7bb7d438155f208a85f1dbe8ba20f77598c9b5d9
wasabeef <dadadada.chop@gmail.com>
2021-12-13 17:28:52 +0900

    test: add verify before rebuild

--- packages/basic/test/use_effect_once_test.dart
+++ packages/basic/test/use_effect_once_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_use/flutter_use.dart';
-import 'flutter_hooks_testing.dart';
 import 'package:mockito/mockito.dart';
+import 'flutter_hooks_testing.dart';
 
 import 'mock.dart';
 
@@ -14,23 +14,19 @@ void main() {
           effect();
         }),
       );
-      await result.rebuild();
       verify(effect()).called(1);
+      await result.rebuild();
+      verifyNever(effect());
       verifyNoMoreInteractions(effect);
     });
 
     testWidgets('should run dispose only once after unmount', (tester) async {
-      final effect = MockEffect();
       final dispose = MockDispose();
       final result = await buildHook(
         (_) => useEffectOnce(() {
-          effect();
           return () => dispose();
         }),
       );
-      verify(effect()).called(1);
-      verifyNoMoreInteractions(effect);
-
       await result.unmount();
       verify(dispose()).called(1);
       verifyNoMoreInteractions(dispose);
