dd0e6f8cb9df28fb6c8d0c7d2518953323bf667a
wasabeef <dadadada.chop@gmail.com>
2021-12-17 15:29:17 +0900

    chore: add example for battery

---
+++ packages/basic/test/use_map_test.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_use/flutter_use.dart';
+// import 'flutter_hooks_testing.dart';
+
+// void main() {
+//   group('useMap', () {
+//     testWidgets('should return an error on error dispatch', (tester) async {
+//       final result = await buildHook((_) => useMap());
+//     });
+//   });
+// }
---
+++ packages/battery/example/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_use_battery/use_battery.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class SampleError extends Error {
+  SampleError(this.message);
+  final String message;
+}
+
+class UseError extends Error {}
+
+class SampleException implements Exception {}
+
+class UseException implements Exception {}
+
+class MyHomePage extends HookWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("build");
+
+    final battery = useBattery();
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("-- Battery --"),
+              Text("fetched: ${battery.fetched}"),
+              Text("batteryState: ${battery.batteryState}"),
+              Text("level: ${battery.batteryLevel}"),
+              Text("isInBatterySaveMode: ${battery.isInBatterySaveMode}"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
